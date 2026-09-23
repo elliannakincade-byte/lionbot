b6eb5b2e0d8ceda51500778ff5a88e7521a4d8fa
Ellianna Kincade <elli.kincade@gmail.com>
2026-09-23 15:19:28 -0500

    Automate LionBot hardware bringup

--- lionbot_bringup/launch/lionbot.launch.py
+++ lionbot_bringup/launch/lionbot.launch.py
@@ -1,6 +1,7 @@
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
@@ -17,8 +18,22 @@ def generate_launch_description():
         'display.launch.py'
     )
 
+    robot_description = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(description_launch)
+    )
+
+    controller_manager = Node(
+        package='controller_manager',
+        executable='ros2_control_node',
+        output='screen',
+        parameters=[
+            {
+                'update_rate': 20
+            }
+        ]
+    )
+
     return LaunchDescription([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(description_launch)
-        )
+        robot_description,
+        controller_manager,
     ])
